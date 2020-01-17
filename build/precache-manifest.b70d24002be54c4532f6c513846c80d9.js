81a3b020d850a82c137a3851421feab262087f21
raman <ramanhira@gmail.com>
2020-01-17 21:25:30 +0530

    added build

---
+++ build/precache-manifest.b70d24002be54c4532f6c513846c80d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f8d26870a1d4dd01f7e976d512ec97",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0075fd2193f15a7da346",
+    "url": "/monster-rolodex/static/css/main.f5e2fec8.chunk.css"
+  },
+  {
+    "revision": "b6b4b7653795c37734cb",
+    "url": "/monster-rolodex/static/js/2.81bb5c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.81bb5c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0075fd2193f15a7da346",
+    "url": "/monster-rolodex/static/js/main.cfa0cfb4.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
