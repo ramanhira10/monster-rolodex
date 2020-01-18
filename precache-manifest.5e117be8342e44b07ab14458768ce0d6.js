46b9962aeb5e9cce8ee241eb6deff22963555c4f
ramanhira10 <ramanhira@gmail.com>
2020-01-18 19:58:02 +0530

    Updates

---
+++ precache-manifest.5e117be8342e44b07ab14458768ce0d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384faad4e6565ff955416e3700ffcf54",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "7e285a0756ba0d07e506",
+    "url": "/monster-rolodex/static/css/main.f5e2fec8.chunk.css"
+  },
+  {
+    "revision": "b6b4b7653795c37734cb",
+    "url": "/monster-rolodex/static/js/2.81bb5c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.81bb5c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e285a0756ba0d07e506",
+    "url": "/monster-rolodex/static/js/main.42346106.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
